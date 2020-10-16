7bddd1fae8b9e3532c349686cb362259ea488c21
martiresalbertoorozcogonzales <martiresalbertoorozcogonzales@gmail.com>
2020-10-16 07:47:05 -0600

    Actualizacion 16/10/2020 Creado el formulario para crear vacantes

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.app')
 
 @section('navegacion')
-@include('ext.adminnav')
-@endsection
+
+ @include('ext.adminnav')
+
+ @endsection()
 
 @section('content')
 
-<h1 class="text-2xl text-center mt-10">Crear Vacante</h1>
+<h1 class="text-2xl text-center mt-10">Nueva Vacante</h1>
 
-@endsection
+    <form class="max-w-lg mx-auto my-10">
+
+        <div class="mb-5">
+            <label for="titulo" class="block text-gray-700 text-sm mb-2">Titulo Vacante</label>
+            
+            <input id="titulo" type="text" class="p-3 bg-white rounded form-input w-full @error('titulo') border-blue-500 border @enderror" name="titulo">
+        </div>
+
+        <button 
+          type="submit"
+          class="bg-blue-500 w-full hover:bg-blue-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">
+          Publicar Vacante
+        </button>
+
+    </form>
+
+@endsection()
